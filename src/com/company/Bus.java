fd3378af12066d4abc7c6576e3f598bfc9f8ac51
Nooruz21 <awbawb2129@gmail.com>
2022-03-08 11:29:37 +0600

    Initial commit

---
+++ src/com/company/Bus.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Bus extends Transport{
+    private String model;
+
+    public Bus(int age, String name, String model) {
+        super(age, name);
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Bus(int age, String name) {
+        super(age, name);
+    }
+
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("Age; 2005");
+    }
+}
